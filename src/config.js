ef6602ac278426f14b39e4b047d978a42de56a04
somebody <some@body.com>
2024-12-18 17:01:05 +0800

    refactor: update game configuration and NPC IDs
    - Changed the GAME_ID from a previous identifier to a new one for the current game.
    - Updated NPC IDs to new values, commenting out the old ones for reference.
    - Added a fifth NPC entry, reflecting the game's character requirements.

--- src/config.js
+++ src/config.js
@@ -1,17 +1,25 @@
-export const GAME_ID = "7411057c-43a0-4fbb-b4b8-f0b02ba3cb02";
-// export const GAME_ID = "GDWSAF9CJ";
+// export const GAME_ID = "7411057c-43a0-4fbb-b4b8-f0b02ba3cb02";
+export const GAME_ID = "GDWSAF9CJ";
 
 export const Characters = {
   npc_01: {
-    id: "1aeaa765-a163-4898-b00b-c9b868e7457d"
+    // id: "1aeaa765-a163-4898-b00b-c9b868e7457d"
+    id: "CDPMQ8OYO"
   },
   npc_02: {
-    id: "8a1244ee-7a19-421f-992e-9a7843dc2cf3"
+    // id: "8a1244ee-7a19-421f-992e-9a7843dc2cf3"
+    id: "CG1TOLMQ5"
   },
   npc_03: {
-    id: "4d8ac888-1997-4671-b0a9-88c530a3f79d"
+    // id: "4d8ac888-1997-4671-b0a9-88c530a3f79d"
+    id: "CSLU3XHEX"
   },
   npc_04: {
-    id: "ec2356af-78c3-4a91-ad33-f3ecfac927f1"
+    // id: "ec2356af-78c3-4a91-ad33-f3ecfac927f1"
+    id: "CI97CJ5A0"
+  },
+  // From Game GDWSAF9CJ, it has 5 NPCs, but only 4 are used in previous game
+  npc_05: {
+    id: "CLSUZWYFH"
   }
 };
